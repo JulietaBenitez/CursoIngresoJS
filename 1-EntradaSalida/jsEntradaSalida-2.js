03655e69afffdcc2bd5b5991fe7ee203b3aa76c9
Julieta Benitez <40808743+JulietaBenitez@users.noreply.github.com>
2018-07-03 21:50:39 -0300

    Primer guardado
    Ejercicios del 1 al 4.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,15 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	//var person = prompt("Please enter your name", "Harry Potter");	
+	//comentario de una linea
+	/* var = variable 
+	*/
+	var nombre; 
+	/*nombre = "Julieta";
+	alert(nombre);
+	*/
+	nombre = prompt ("Ingrese nombre", "Natalia Natalia");
+	alert("Su nombre es:" + nombre);
 }
 

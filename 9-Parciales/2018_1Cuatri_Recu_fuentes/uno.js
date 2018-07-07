1d505933a1b62c9663aeb5dacafa8d0a4528cdc2
Julieta Benitez <40808743+JulietaBenitez@users.noreply.github.com>
2018-07-06 22:22:13 -0300

    Ejercicio sin terminar
    Terminarlo.

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,26 @@
 
+/* Se ingresan tres numeros.
+ 	Se muestra: la suma de los tres, el promedio de los tres
+ 	y la resta de los tres en un unico alert.*/
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var numeroTres;
+	var suma;
+	var promedio;
+	var resta;
+
+	resultado = ;
+	numeroUno = prompt("numeroUno");
+	numeroUno = parseInt(numeroUno);
+	numeroDos = prompt("numeroDos");
+	numeroDos = parseInt(numeroDos);
+	numeroTres = prompt ("numeroTres");
+	numeroTres= parseInt (numeroTres);
+
+	alert ()
+
+
 
 }
